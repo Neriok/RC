6297d867643f2e2fac1e3cd8a353ad10cb1acd89
Leonardo Larrad <larradleonardo@gmail.com>
2018-01-01 20:27:58 -0400

    Add files via upload

---
+++ Source/Data/Database/RcDatabase.cs
@@ -0,0 +1,18 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC.Data.Database
+{
+    /// <summary>
+    /// Represents RC database with SQLite.
+    /// </summary>
+    public class RcDatabase
+    {
+
+
+    }
+}
