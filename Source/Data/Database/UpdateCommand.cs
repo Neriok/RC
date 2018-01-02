5a6eb0fa6c26e37dddc69dbd2bcbfff60ee55e7f
Leonardo Larrad <larradleonardo@gmail.com>
2018-01-01 20:27:17 -0400

    Add files via upload

---
+++ Source/Data/Database/UpdateCommand.cs
@@ -0,0 +1,58 @@
+﻿using Mono.Data.Sqlite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RC.Data.Database
+{
+    /// <summary>
+	/// Wrapper around SqliteCommand, for easier, cleaner updating.
+	/// </summary>
+    public class UpdateCommand : SimpleCommand
+    {
+        // --------------------------------------------------
+        // Constructor
+        // --------------------------------------------------
+
+        /// <summary>
+		/// Creates new update command.
+		/// </summary>		
+		public UpdateCommand(string command, SqliteConnection connection, SqliteTransaction transaction = null)
+            : base(command, connection, transaction)
+        {
+        }
+
+        // --------------------------------------------------
+        // Methods
+        // --------------------------------------------------
+
+        /// <summary>
+		/// Runs SqliteCommand.ExecuteNonQuery
+		/// </summary>
+		/// <returns></returns>
+		public override int Execute()
+        {
+            // Build setting part
+            StringBuilder sb = new StringBuilder();
+            foreach (String parameter in _set.Keys)
+            {
+                sb.AppendFormat("`{0}` = @{0}, ", parameter);
+            }               
+
+            // Add setting part
+            _command.CommandText = string.Format(_command.CommandText, sb.ToString().Trim(' ', ','));
+
+            // Add parameters
+            foreach (KeyValuePair<String, Object> parameter in _set)
+            {
+                _command.Parameters.AddWithValue("@" + parameter.Key, parameter.Value);
+            }
+            
+            // Run
+            return _command.ExecuteNonQuery();
+        }
+
+    }
+}
