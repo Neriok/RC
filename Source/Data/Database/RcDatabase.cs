06c45327687122cf0b5f92fa6e6e4ace0c3fbe58
Leonardo Larrad <larradleonardo@gmail.com>
2018-01-02 00:29:26 -0400

    Add base connection

--- Source/Data/Database/RcDatabase.cs
+++ Source/Data/Database/RcDatabase.cs
@@ -12,7 +12,85 @@ namespace RC.Data.Database
     /// </summary>
     public class RcDatabase
     {
+        // --------------------------------------------------
+        // Fields
+        // --------------------------------------------------
+
+        private string _connectionString;
+
+        // --------------------------------------------------
+        // Properties
+        // --------------------------------------------------
+
+        /// <summary>
+        /// Gets the connection string path. 
+        /// </summary>
+        public String ConnectionString
+        {
+            get
+            {
+                return _connectionString;
+            }
+        }
+
+        // --------------------------------------------------
+        // Methods
+        // --------------------------------------------------
+
+        /// <summary>
+        /// Sets connection string and calls TestConnection.
+        /// </summary>
+        public void Init(String connectionString)
+        {
+            _connectionString = connectionString;
+            TestConnection();
+        }
+
+        /// <summary>
+		/// Returns a valid connection.
+		/// </summary>
+        protected SqliteConnection GetConnection()
+        {
+            if(_connectionString == null)
+            {
+                throw new Exception("Database has not been initialized.");
+            }
+
+            SqliteConnection connection = new SqliteConnection("URI=file:" + _connectionString);
+            connection.Open();
+
+            // Active foreing keys.
+            using (SqliteCommand command = new SqliteCommand("PRAGMA foreign_keys = 1", connection))
+            {
+                command.ExecuteNonQuery();
+            }
 
 
+            return connection;
+        }
+
+        /// <summary>
+		/// Tests connection.
+		/// </summary>
+		/// <exception cref="Exception">Thrown if connection couldn't be established.</exception>
+        public void TestConnection()
+        {
+            SqliteConnection connection = null;
+
+            try
+            {
+                connection = this.GetConnection();
+            }
+            finally
+            {
+                if(connection != null)
+                {
+                    connection.Close();
+                }
+            }
+
+        }
+
+        
     }
 }
